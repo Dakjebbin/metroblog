696cc1887c952ac9e644b519d6f2467a7ef4c518
DakJebbin <Jebbinp@gmail.com>
2025-03-07 12:35:07 +0100

    news section

---
+++ bb-blog/src/components/News.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <section className='m-auto w-[95%]'> 
+       <div className="our-courses  "> 
+        <hr /> 
+      <span className='instrument-serif-regular text-xl'> NEWS </span>
+      <hr /> 
+       </div>
+    </section>
+  )
+}
+
+export default News
--- bb-blog/src/page/Home.tsx
+++ bb-blog/src/page/Home.tsx
@@ -1,12 +1,14 @@
 
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import News from '../components/News'
 
 const Home = () => {
   return (
     <div>
         <Header/>
         <Hero/>
+        <News/>
     </div>
   )
 }
